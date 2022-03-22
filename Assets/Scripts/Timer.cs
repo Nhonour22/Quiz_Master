36a16f8db848cf4a81f5a5380aa4cc436ef8f68e
Nate Honour <Honour067@morrisville.edu>
2022-03-22 14:40:22 -0400

    Simple Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+
+   public bool loadNextQuestion;
+   public float fillFraction;
+   
+   bool isAnsweringQustion; 
+    float timerValue;
+    
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if(isAnsweringQustion)
+        {
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                isAnsweringQustion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
+                isAnsweringQustion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
+        }
+    }
+}
